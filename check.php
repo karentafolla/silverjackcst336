b5e622df2ccefe028f7f4576245d1ee50eb8fdc4
mirandasaari <miranda.saari@gmail.com>
2016-02-26 23:05:18 +0000

    cards arrays

---
+++ check.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+<p>
+    <?php echo $_POST("day");?>
+</p>
+    </body>
+</html>
--- game.php
+++ game.php
@@ -2,12 +2,174 @@
 
 
 <?php
-    // $card = ["imageName" => "",
-    //          "score" => "",
-    //          "suit" => "",
-    //          "rank" => ""];
-    // //indexed array         
+//clubs
+     $club1= ["imageName" => '<img src="assets/clubs/1.png"/>',
+             "score" => "1",
+             "suit" => "clubs"];
+     $club2= ["imageName" => '<img src="assets/clubs/2.png"/>',
+             "score" => "2",
+             "suit" => "clubs"];
+     $club3= ["imageName" => '<img src="assets/clubs/3.png"/>',
+             "score" => "3",
+             "suit" => "clubs"];
+     $club4= ["imageName" => '<img src="assets/clubs/4.png"/>',
+             "score" => "4",
+             "suit" => "clubs"];
+     $club5= ["imageName" => '<img src="assets/clubs/5.png"/>',
+             "score" => "5",
+             "suit" => "clubs"];
+     $club6= ["imageName" => '<img src="assets/clubs/6.png"/>',
+             "score" => "6",
+             "suit" => "clubs"];
+     $club7 = ["imageName" => '<img src="assets/clubs/7.png"/>',
+             "score" => "7",
+             "suit" => "clubs"];
+    $club8 = ["imageName" => '<img src="assets/clubs/8.png"/>',
+             "score" => "8",
+             "suit" => "clubs"];
+    $club9= ["imageName" => '<img src="assets/clubs/9.png"/>',
+             "score" => "3",
+             "suit" => "clubs"];
+    $club10 = ["imageName" => '<img src="assets/clubs/10.png"/>',
+             "score" => "10",
+             "suit" => "clubs"];
+    $clubjack = ["imageName" => '<img src="assets/clubs/11.png"/>',
+             "score" => "11",
+             "suit" => "clubs"];
+    $clubqueen = ["imageName" => '<img src="assets/clubs/12.png"/>',
+             "score" => "12",
+             "suit" => "clubs"];
+    $clubking = ["imageName" => '<img src="assets/clubs/13.png"/>',
+             "score" => "13",
+             "suit" => "clubs"];
+             
+//Diamonds
+    $diamonds1=["imageName"=>'<img src="assets/diamonds/1.png"/>',
+        "score"=>"1",
+        "suit"=>"diamonds"];
+    $diamonds2=["imageName"=>'<img src="assets/diamonds/2.png"/>',
+        "score"=>"2",
+        "suit"=>"diamonds"];
+    $diamonds3=["imageName"=>'<img src="assets/diamonds/3.png"/>',
+        "score"=>"3",
+        "suit"=>"diamonds"];
+    $diamonds4=["imageName"=>'<img src="assets/diamonds/4.png"/>',
+        "score"=>"4",
+        "suit"=>"diamonds"];
+    $diamonds5=["imageName"=>'<img src="assets/diamonds/5.png"/>',
+        "score"=>"5",
+        "suit"=>"diamonds"];
+    $diamonds6=["imageName"=>'<img src="assets/diamonds/6.png"/>',
+        "score"=>"6",
+        "suit"=>"diamonds"];
+    $diamonds7=["imageName"=>'<img src="assets/diamonds/7.png"/>',
+        "score"=>"7",
+        "suit"=>"diamonds"];
+    $diamonds8=["imageName"=>'<img src="assets/diamonds/8.png"/>',
+        "score"=>"8",
+        "suit"=>"diamonds"];
+    $diamonds9=["imageName"=>'<img src="assets/diamonds/9.png"/>',
+        "score"=>"9",
+        "suit"=>"diamonds"];
+    $diamonds10=["imageName"=>'<img src="assets/diamonds/10.png"/>',
+        "score"=>"10",
+        "suit"=>"diamonds"];
+    $diamondsJack=["imageName"=>'<img src="assets/diamonds/11.png"/>',
+        "score"=>"11",
+        "suit"=>"diamonds"];
+    $diamondsQueen=["imageName"=>'<img src="assets/diamonds/12.png"/>',
+        "score"=>"12",
+        "suit"=>"diamonds"];
+    $diamondsKing=["imageName"=>'<img src="assets/diamonds/13.png"/>',
+        "score"=>"13",
+        "suit"=>"diamonds"];
+//Hearts
+    $hearts1=["imageName"=>'<img src="assets/hearts/1.png"/>',
+        "score"=>"1",
+        "suit"=>"hearts"];
+    $hearts2=["imageName"=>'<img src="assets/hearts/2.png"/>',
+        "score"=>"2",
+        "suit"=>"hearts"];
+    $hearts3=["imageName"=>'<img src="assets/hearts/3.png"/>',
+        "score"=>"3",
+        "suit"=>"hearts"];
+    $hearts4=["imageName"=>'<img src="assets/hearts/4.png"/>',
+        "score"=>"4",
+        "suit"=>"hearts"];
+    $hearts5=["imageName"=>'<img src="assets/hearts/5.png"/>',
+        "score"=>"5",
+        "suit"=>"hearts"];
+    $hearts6=["imageName"=>'<img src="assets/hearts/6.png"/>',
+        "score"=>"6",
+        "suit"=>"hearts"];
+    $hearts7=["imageName"=>'<img src="assets/hearts/7.png"/>',
+        "score"=>"7",
+        "suit"=>"hearts"];
+    $hearts8=["imageName"=>'<img src="assets/hearts/8.png"/>',
+        "score"=>"8",
+        "suit"=>"hearts"];
+    $hearts9=["imageName"=>'<img src="assets/hearts/9.png"/>',
+        "score"=>"9",
+        "suit"=>"hearts"];
+    $hearts10=["imageName"=>'<img src="assets/hearts/10.png"/>',
+        "score"=>"10",
+        "suit"=>"hearts"];
+    $heartsJack=["imageName"=>'<img src="assets/hearts/11.png"/>',
+        "score"=>"11",
+        "suit"=>"hearts"];
+    $heartsQueen=["imageName"=>'<img src="assets/hearts/12.png"/>',
+        "score"=>"12",
+        "suit"=>"hearts"];
+    $heartsKing=["imageName"=>'<img src="assets/hearts/13.png"/>',
+        "score"=>"13",
+        "suit"=>"hearts"];
+        
+//Spades
+     $spades1=["imageName"=>'<img src="assets/spades/1.png"/>',
+        "score"=>"1",
+        "suit"=>"Spades"];
+     $spades2=["imageName"=>'<img src="assets/spades/2.png"/>',
+        "score"=>"2",
+        "suit"=>"Spades"];
+     $spades3=["imageName"=>'<img src="assets/spades/3.png"/>',
+        "score"=>"3",
+        "suit"=>"Spades"];
+     $spades4=["imageName"=>'<img src="assets/spades/4.png"/>',
+        "score"=>"4",
+        "suit"=>"Spades"];
+     $spades5=["imageName"=>'<img src="assets/spades/5.png"/>',
+        "score"=>"5",
+        "suit"=>"Spades"];
+     $spades6=["imageName"=>'<img src="assets/spades/6.png"/>',
+        "score"=>"6",
+        "suit"=>"Spades"];
+     $spades7=["imageName"=>'<img src="assets/spades/7.png"/>',
+        "score"=>"7",
+        "suit"=>"Spades"];
+     $spades8=["imageName"=>'<img src="assets/spades/8.png"/>',
+        "score"=>"8",
+        "suit"=>"Spades"];
+     $spades9=["imageName"=>'<img src="assets/spades/9.png"/>',
+        "score"=>"9",
+        "suit"=>"Spades"];
+     $spades10=["imageName"=>'<img src="assets/spades/10.png"/>',
+        "score"=>"10",
+        "suit"=>"Spades"];
+     $spadesJack=["imageName"=>'<img src="assets/spades/11.png"/>',
+        "score"=>"11",
+        "suit"=>"Spades"];
+     $spadesQueen=["imageName"=>'<img src="assets/spades/12.png"/>',
+        "score"=>"12",
+        "suit"=>"Spades"];
+     $spadesKing=["imageName"=>'<img src="assets/spades/13.png"/>',
+        "score"=>"13",
+        "suit"=>"Spades"];
+
+
+    //indexed array         
     // $deck = [];
+    
+    echo  $club1["imageName"];
     
     //assosiative array
     $player1 =["imageName" => "",
